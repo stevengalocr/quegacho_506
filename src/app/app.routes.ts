0efee0100eb2a437d1abe0ce7c0165e2fad01155
Steven Galo <stevengalocr@gmail.com>
2025-01-19 10:19:56 -0500

    Add section 222

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,8 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { SectionDetailsComponent } from './pages/details/section-details.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
-//   { path: 'tienda', component: StoreComponent },
-//   { path: 'nosotros', component: AboutUsComponent }
+  { path: 'details', component: SectionDetailsComponent },
 ];
---
+++ src/app/pages/details/section-details.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-section-details',
+  standalone: true,
+  templateUrl: './section-details.component.html',
+  styleUrls: ['./section-details.component.scss'],
+})
+export class SectionDetailsComponent {}
